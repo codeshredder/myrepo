a72c6392b69820bd8e561db03fef9ae851298d02
codeshredder <evilforce@gmail.com>
2015-09-06 11:23:02 +0800

    Update file2.go

--- code/go/file2.go
+++ code/go/file2.go
@@ -1,16 +1,27 @@
 package main
+
 import "io/ioutil"
+import "os"
+import "fmt"
+
 func listAll(path string) {
     files, _ := ioutil.ReadDir(path)
     for _, fi := range files {
         if fi.IsDir() {
-        //listAll(path + "/" + fi.Name())
-        println(path + "/" + fi.Name())
-    } else {
-        println(path + "/" + fi.Name())
-    }
+            println("into:" + path + "/" + fi.Name())
+            listAll(path + "/" + fi.Name())
+        } else {
+            println(path + fi.Name())
+        }
     }
 }
 func main(){
-    listAll(".")
+
+    fmt.Printf("args:[%d][%s][%s]\n", len(os.Args), os.Args[0], os.Args[1])
+
+    if len(os.Args) > 1 {
+        listAll(os.Args[1])
+    }else {
+	listAll(".")
+    }
 }
