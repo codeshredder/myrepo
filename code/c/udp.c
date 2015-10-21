15bfb6b7c9672e95b582bcaaf683cb43bd500913
codeshredder <evilforce@gmail.com>
2015-10-21 10:41:46 +0800

    Update udp.c

--- code/c/udp.c
+++ code/c/udp.c
@@ -17,75 +17,101 @@
 #define BUFLEN    2048
 #define PORT      8888
 
+void *recv_task(void *arg)
+{
+	int sockfd = *(int *)arg;
+	struct sockaddr_in addr;
+	int addrlen = sizeof(addr);
+	char buffer[BUFLEN];
+	int buffer_len = 0;
+
+	while(1) {
+		memset(buffer, 0, BUFLEN);
+		if ((buffer_len = recvfrom(sockfd, buffer, BUFLEN, 0, (struct sockaddr *) &addr, &addrlen)) < 0)
+		{
+		    printf("recvfrom error: %s(errno: %d)\n", strerror(errno), errno);
+			continue;
+		}
+
+		printf("recvfrom %s:%d\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
+		printf("data: %s\n" , buffer);
+	}
+}
 
 int main(int argc, char**argv)
 {
-        int sockfd;
-        struct sockaddr_in seraddr;
-        struct sockaddr_in cliaddr;
-        int serlen = sizeof(seraddr);
-        int clilen = sizeof(cliaddr);
+	int sockfd;
+	struct sockaddr_in addr;
+	int addrlen = sizeof(addr);
+	char buffer[BUFLEN];
+	int buffer_len = 0;
 
-        char buffer[BUFLEN];
-        int buffer_len;
-        int i;
-
-        //create a UDP socket
-        if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-        {
-        printf("create socket error: %s(errno: %d)\n", strerror(errno), errno);
-        return -1;
-        }
-
-        // zero out the structure
-        memset((char *) &seraddr, 0, sizeof(seraddr));
-
-        seraddr.sin_family = AF_INET;
-        seraddr.sin_port = htons(PORT);
-        seraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-        //bind socket to port
-        if( bind(sockfd , (struct sockaddr*)&seraddr, sizeof(seraddr) ) == -1)
-        {
-        printf("bind error: %s(errno: %d)\n", strerror(errno), errno);
-        return -1;
+    if (argc < 2) {
+		return 0;
     }
 
-        //keep listening for data
-        while(1)
-        {
-#if 1
-                memset(buffer, 0, BUFLEN);
+    if (0 == strcmp(argv[1],"server")) {
+
+	    pthread_t task_thread;
+
+		if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+		{
+	        printf("create socket error: %s(errno: %d)\n", strerror(errno), errno);
+	        return -1;
+		}
+		
+		memset((char *) &addr, 0, sizeof(addr));
+		addr.sin_family = AF_INET;
+		addr.sin_port = htons(PORT);              // listen port
+		addr.sin_addr.s_addr = htonl(INADDR_ANY);
+		
+		//bind socket to port
+		if( bind(sockfd , (struct sockaddr*)&addr, sizeof(addr) ) == -1)
+		{
+	        printf("bind error: %s(errno: %d)\n", strerror(errno), errno);
+      		close(sockfd);
+	        return -1;
+	    }
+
+	    if (pthread_create(&task_thread, NULL, recv_task, &sockfd) < 0)
+	    {
+	        printf("pthread_create failed!\r\n");
+      		close(sockfd);
+	        return -1;
+	    }
+
+	    pthread_join(task_thread, NULL);
+
+	    close(sockfd);
+	}
+	else if (0 == strcmp(argv[1],"client")) {
+
+		if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+		{
+	        printf("create socket error: %s(errno: %d)\n", strerror(errno), errno);
+	        return -1;
+		}
+
+		memset(buffer, 0, BUFLEN);
         strncpy(buffer, "aaa", BUFLEN);
-        buffer_len = strlen(buffer);
+    	buffer_len = strlen(buffer);
 
-        cliaddr.sin_family = AF_INET;
-        cliaddr.sin_port = htons(PORT);
-        cliaddr.sin_addr.s_addr = inet_addr("172.22.13.100");
+    	addr.sin_family = AF_INET;
+    	addr.sin_port = htons(PORT);               // dest port
+    	addr.sin_addr.s_addr = inet_addr(argv[2]); //dest ip
 
-                if (sendto(sockfd, buffer, buffer_len, 0, (struct sockaddr*) &cliaddr, clilen) < -1)
-                {
-                    printf("sendto error: %s(errno: %d)\n", strerror(errno), errno);
-                        continue;
-                }
-#endif
+		if (sendto(sockfd, buffer, buffer_len, 0, (struct sockaddr*) &addr, addrlen) < -1)
+		{
+		    printf("sendto error: %s(errno: %d)\n", strerror(errno), errno);
+			close(sockfd);
+			return -1;
+		}
 
-#if 1
-                printf("Waiting for data...");
-                fflush(stdout);
+		printf("sendto %s:%d\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
+		printf("data: %s\n" , buffer);
 
-                memset(buffer, 0, BUFLEN);
-                if ((buffer_len = recvfrom(sockfd, buffer, BUFLEN, 0, (struct sockaddr *) &cliaddr, &clilen)) < 0)
-                {
-                    printf("recvfrom error: %s(errno: %d)\n", strerror(errno), errno);
-                        continue;
-                }
-
-                printf("recvfrom %s:%d\n", inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port));
-                printf("Data: %s\n" , buffer);
-#endif
-        }
-
-        close(sockfd);
-        return 0;
+		close(sockfd);
+	}
+    
+    return 0;
 }
